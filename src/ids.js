cd0aa7e1461829cf6fc4f73b2e45efb9ad32ee05
Ibrahim Muhammad Jafar <jafaribrahimm959@gmail.com>
2026-09-23 13:13:44 +0100

    Replace old app with Relics

--- src/ids.js
+++ src/ids.js
@@ -2,14 +2,23 @@ export const NETWORK = "mainnet";
 export const DEPLOYER =
   "0x15c87c2c26478e5e7da775cc515529bdb393050c28889e25363a71882759ff9f";
 
-// Fill these after the mainnet package is published.
-export const PKG = "";
-export const CONFIG = "";
-export const RECORD = "";
+export const PKG =
+  "0x8e08915671e2617f76f3b93945056b87e0a23086e9b065f64b522f2502aee46b";
+export const CONFIG =
+  "0x8f6765bde7c3599bc75e67ea3e0cba7ad7e15c68c9c970c2ccb4efb0229ab1ea";
+export const RECORD =
+  "0xc829e0b6c25602702ec96cf0039e302b2e37664b7aee3863f2d741e0c5677f9c";
+
 export const TOKEN_CONFIG = "";
 export const RELIC_TYPE = "";
-export const RELIC_NFT = "";
-export const MERGE_DEAL = "";
+export const RELIC_NFT = `${PKG}::relics::Relic`;
+export const MERGE_DEAL = `${PKG}::relics::MergeDeal`;
+export const MERGE_REQUEST = `${PKG}::relics::MergeRequest`;
+
+export const ADMIN_CAP =
+  "0x9ff0a65d88b1268f3d8951430ff880425ce419e48fc48bd24648adec25818ceb";
+export const UPGRADE_CAP =
+  "0xf833e9c8ba57dc991d7cc36638a15754073dc9e8d2f3c8923bb8780150521e87";
 
 export const ART = {
   1: "https://aggregator.walrus-mainnet.walrus.space/v1/blobs/by-object-id/0xb497c56ada9b1db879144b02c4923c760e92a1a87e40deb4a62b3940872a5bff",
